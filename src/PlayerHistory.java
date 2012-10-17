74d0bd6aca7ed344696a5deb5dfe3d72b3721d4d
vinayakbansal <vinayak_bansal@carleton.ca>
2012-10-17 13:06:58 -0400

    Adding skeleton code

--- src/PlayerHistory.java
+++ src/PlayerHistory.java
@@ -1,4 +1,26 @@
+import java.util.Stack;
+
 
 public class PlayerHistory {
 
+	private Stack undoStack;
+	private Stack redoStack;
+	
+	public void addStep( Command c) {
+		
+	}
+	
+	public Command undo() {
+		return null;
+	}
+	
+	public Command redo() {
+		return null;
+	}
+	
+	public void clear() {
+		
+	}
+	
+	
 }
