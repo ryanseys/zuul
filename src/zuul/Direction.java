4c24fdc6e7f3ddfd2d6d35e3bde462caaa88e82d
vinayakbansal <vinayak_bansal@carleton.ca>
2012-11-22 09:36:21 -0500

    Being able to save Command, CommandWords, and Direction Add tests in CommandTest and DirectionTest Fix typos in ItemTest

--- src/zuul/Direction.java
+++ src/zuul/Direction.java
@@ -1,5 +1,9 @@
 package zuul;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * Direction class represents all the ways that a room can be placed relative to
  * another room. Effectively, this also represents all the ways that a person
@@ -24,5 +28,13 @@ public enum Direction {
     else if (equals(Direction.SOUTH)) return Direction.NORTH;
     else if (equals(Direction.WEST)) return Direction.EAST;
     else return null;
+  }
+  
+  public void save(ObjectOutputStream input) throws IOException {
+	  input.writeUTF(this.name());
+  }
+  
+  public static Direction retrieve(ObjectInputStream input) throws IOException {
+	  return Direction.valueOf(input.readUTF());
   }
 }
