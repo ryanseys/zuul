a4469844f56cbfc40073665de48f4d0631ed21ee
vinayakbansal <vinayak_bansal@carleton.ca>
2012-11-22 09:07:00 -0500

    Able to save items Add a test to ensure items are saved properly

--- src/Test/ItemTest.java
+++ src/Test/ItemTest.java
@@ -3,6 +3,11 @@ package Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,30 +16,50 @@ import zuul.Item;
 
 public class ItemTest {
 
-  Item i;
-  Item i3;
+	Item i;
+	Item i3;
 
-  @Before
-  public void setUp() throws Exception {
-    i = new Item("Sword", 10, 10, true);
-    i3 = new Item("Axe", 30, 10, true);
-  }
+	@Before
+	public void setUp() throws Exception {
+		i = new Item("Sword", 10, 10, true);
+		i3 = new Item("Axe", 30, 10, true);
+	}
 
-  @After
-  public void tearDown() throws Exception {
-  }
+	@After
+	public void tearDown() throws Exception {
+	}
 
-  @Test
-  public void testCompareTo() {
-    assertTrue(i.compareTo(i3) == -1);
-    assertTrue(i3.compareTo(i) == 1);
-    assertTrue(i.compareTo(i) == 0);
-  }
+	@Test
+	public void testCompareTo() {
+		assertTrue(i.compareTo(i3) == -1);
+		assertTrue(i3.compareTo(i) == 1);
+		assertTrue(i.compareTo(i) == 0);
+	}
 
-  @Test
-  public void testEqualsObject() {
-    Item i2 = new Item("Sword", 10, 10, true);
-    assertTrue(i.equals(i2));
-    assertFalse(i.equals(i3));
-  }
+	@Test
+	public void testEqualsObject() {
+		Item i2 = new Item("Sword", 10, 10, true);
+		assertTrue(i.equals(i2));
+		assertFalse(i.equals(i3));
+	}
+
+	@Test
+	public void testSaveRetrieve() {
+		FileOutputStream fos;
+		ObjectOutputStream oos;
+		ObjectInputStream in;
+		try {
+			fos = new FileOutputStream("myFile.txt");
+			oos = new ObjectOutputStream(fos);
+			in = new ObjectInputStream(new FileInputStream("myFile.txt"));
+			i.save(oos);
+			oos.close();
+			Item retrieve = Item.retrieve(in);
+			assertTrue(i.equals(retrieve));
+			assertFalse(i.equals(13));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- src/zuul/Item.java
+++ src/zuul/Item.java
@@ -1,5 +1,10 @@
 package zuul;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 /**
  * Item class. This class deals with the individual items. Items are given a
  * name, a value, and a weight. The name is simply the name of the item. The
@@ -15,168 +20,188 @@ package zuul;
  * @version 2012.10.23
  */
 
-public class Item implements Comparable<Item> {
+public class Item implements Comparable<Item>, Serializable {
 
-  private String name;
-  private int value;
-  private int weight;
-  private boolean isWeapon;
+	private String name;
+	private int value;
+	private int weight;
+	private boolean isWeapon;
 
-  /**
-   * Default constructor. Creates an item with a name with no value or weight
-   * 
-   * @param name
-   *          : The name of the item.
-   * @param isWeapon
-   *          : Whether or not it is a weapon
-   */
-  public Item(String name, boolean isWeapon) {
-    this(name, 0, 0, isWeapon);
-  }
+	/**
+	 * Default constructor. Creates an item with a name with no value or weight
+	 * 
+	 * @param name
+	 *            : The name of the item.
+	 * @param isWeapon
+	 *            : Whether or not it is a weapon
+	 */
+	public Item(String name, boolean isWeapon) {
+		this(name, 0, 0, isWeapon);
+	}
 
-  /**
-   * Constructor for the item.
-   * 
-   * @param name
-   *          : The name of the item being created.
-   * @param value
-   *          : The value of the item.
-   * @param weight
-   *          : The weight of the item.
-   * @param isWeapon
-   *          : Whether or not it is a weapon
-   */
-  public Item(String name, int value, int weight, boolean isWeapon) {
-    // Constructor assigning the variables
-    this.name = name.toUpperCase();
-    this.value = value;
-    this.weight = weight;
-    this.isWeapon = isWeapon;
-  }
+	/**
+	 * Constructor for the item.
+	 * 
+	 * @param name
+	 *            : The name of the item being created.
+	 * @param value
+	 *            : The value of the item.
+	 * @param weight
+	 *            : The weight of the item.
+	 * @param isWeapon
+	 *            : Whether or not it is a weapon
+	 */
+	public Item(String name, int value, int weight, boolean isWeapon) {
+		// Constructor assigning the variables
+		this.name = name.toUpperCase();
+		this.value = value;
+		this.weight = weight;
+		this.isWeapon = isWeapon;
+	}
 
-  /**
-   * Overriding the compareTo method in Comparable.
-   * 
-   * @param i
-   *          : The passed item to compare.
-   * @return : This method returns 1 if the value of this item is greater than
-   *         the one passed. It returns 0 if the items have the same value. It
-   *         return -1 if the passed item is greater than this item.
-   */
-  @Override
-  public int compareTo(Item i) {
-    if (value > i.value) return 1; // this item is of greater value than the
-                                   // parameter item
-    else if (value == i.value) return 0; // the items have the same value (tie)
-    // otherwise, this.value < i.value
-    return -1; // this item is of less value than the parameter item
-  }
+	/**
+	 * Overriding the compareTo method in Comparable.
+	 * 
+	 * @param i
+	 *            : The passed item to compare.
+	 * @return : This method returns 1 if the value of this item is greater than
+	 *         the one passed. It returns 0 if the items have the same value. It
+	 *         return -1 if the passed item is greater than this item.
+	 */
+	@Override
+	public int compareTo(Item i) {
+		if (value > i.value)
+			return 1; // this item is of greater value than the
+						// parameter item
+		else if (value == i.value)
+			return 0; // the items have the same value (tie)
+		// otherwise, this.value < i.value
+		return -1; // this item is of less value than the parameter item
+	}
 
-  /**
-   * Overriding the equals method, to check if items are the same.
-   * 
-   * @param o
-   *          : An object passed to check for equality
-   * @return : Returns a boolean whether or not the items are the same.
-   */
-  @Override
-  public boolean equals(Object o) {
-    if (o instanceof Item) return (name.equals(((Item) o).getName()));
-    return false;
-  }
+	/**
+	 * Overriding the equals method, to check if items are the same.
+	 * 
+	 * @param o
+	 *            : An object passed to check for equality
+	 * @return : Returns a boolean whether or not the items are the same.
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Item)
+			return (name.equals(((Item) o).getName()));
+		return false;
+	}
 
-  /**
-   * Get the description of the item. Either add a or an to the string depending
-   * on if it is a consonant or not.
-   * 
-   * @return : The string giving the required information for inspection of an
-   *         item.
-   */
-  public String getDescription() {
-    String s = "This is ";
-    if (name.startsWith("A") || name.startsWith("E") || name.startsWith("I")
-        || name.startsWith("O") || name.startsWith("U")) s += "an ";
-    else s += "a ";
-    s += name.toLowerCase() + ".\n";
-    if (isWeapon()) s += "It can do " + value + " damage when used to fight.";
-    else s += "It can recover " + value + " health when eaten.";
+	/**
+	 * Get the description of the item. Either add a or an to the string
+	 * depending on if it is a consonant or not.
+	 * 
+	 * @return : The string giving the required information for inspection of an
+	 *         item.
+	 */
+	public String getDescription() {
+		String s = "This is ";
+		if (name.startsWith("A") || name.startsWith("E")
+				|| name.startsWith("I") || name.startsWith("O")
+				|| name.startsWith("U"))
+			s += "an ";
+		else
+			s += "a ";
+		s += name.toLowerCase() + ".\n";
+		if (isWeapon())
+			s += "It can do " + value + " damage when used to fight.";
+		else
+			s += "It can recover " + value + " health when eaten.";
 
-    return s;
-  }
+		return s;
+	}
 
-  /**
-   * Getter for the name.
-   * 
-   * @return : The name of the item.
-   */
-  public String getName() {
-    return name;
-  }
+	/**
+	 * Getter for the name.
+	 * 
+	 * @return : The name of the item.
+	 */
+	public String getName() {
+		return name;
+	}
 
-  /**
-   * Getter for the value.
-   * 
-   * @return : The value of the item.
-   */
-  public int getValue() {
-    return value;
-  }
+	/**
+	 * Getter for the value.
+	 * 
+	 * @return : The value of the item.
+	 */
+	public int getValue() {
+		return value;
+	}
 
-  /**
-   * Getter for the weight.
-   * 
-   * @return : The weight of the item.
-   */
-  public int getWeight() {
-    return weight;
-  }
+	/**
+	 * Getter for the weight.
+	 * 
+	 * @return : The weight of the item.
+	 */
+	public int getWeight() {
+		return weight;
+	}
 
-  /**
-   * Query for the item.
-   * 
-   * @return : Whether is not the item is a weapon.
-   */
-  public boolean isWeapon() {
-    return isWeapon;
-  }
+	/**
+	 * Query for the item.
+	 * 
+	 * @return : Whether is not the item is a weapon.
+	 */
+	public boolean isWeapon() {
+		return isWeapon;
+	}
 
-  /**
-   * Setter for the name.
-   * 
-   * @param name
-   *          : The new name of the item.
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
+	/**
+	 * Setter for the name.
+	 * 
+	 * @param name
+	 *            : The new name of the item.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  /**
-   * Setter for the value.
-   * 
-   * @param value
-   *          : The new value of the item.
-   */
-  public void setValue(int value) {
-    this.value = value;
-  }
+	/**
+	 * Setter for the value.
+	 * 
+	 * @param value
+	 *            : The new value of the item.
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
 
-  /**
-   * Setter for the weight.
-   * 
-   * @param weight
-   *          : The new weight of the item.
-   */
-  public void setWeight(int weight) {
-    this.weight = weight;
-  }
+	/**
+	 * Setter for the weight.
+	 * 
+	 * @param weight
+	 *            : The new weight of the item.
+	 */
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
 
-  /**
-   * The toString method for an item which just returns it's name.
-   * 
-   * @return : The name of the item.
-   */
-  @Override
-  public String toString() {
-    return name;
-  }
+	/**
+	 * The toString method for an item which just returns it's name.
+	 * 
+	 * @return : The name of the item.
+	 */
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	public void save(ObjectOutputStream input) throws IOException {
+		input.writeUTF(name);
+		input.writeInt(value);
+		input.writeInt(weight);
+		input.writeBoolean(isWeapon);
+	}
+
+	public static Item retrieve(ObjectInputStream input) throws IOException {
+		return new Item(input.readUTF(), input.readInt(), input.readInt(),
+				input.readBoolean());
+	}
 }
