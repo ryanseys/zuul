cae00777c20adf7a8736ede9ae05e72cf48fd468
Jarred Linthorne-Shaw <jarred.linthorne@gmail.com>
2012-11-24 01:19:09 -0500

    Create GameBuilder class. You can open this GUI and select which rooms you want to have in your game. The GUI is able to select and deselect rooms. There is a button to signify when the user is done selecting. The mapBuilder is called and functions correctly, updating itself as the room gets changed about.

--- src/mapBuilder.java
+++ src/mapBuilder.java
@@ -1,107 +1,103 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
 import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.print.DocFlavor.URL;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
 
-
+/**
+ * This class will create an image of the minimap to be used in the View.
+ * @author Jarred
+ *
+ */
 public class mapBuilder extends JFrame {
 
-	  BufferedImage image = null, overlay = null;
-	  BufferedImage combined;
-	  BufferedImage player;
-		File path = new File("Images");
-		static boolean[] b;
-		int i = 0;
-		static int pp = 9; //player position
-		public mapBuilder(boolean[] b, int room){
+	BufferedImage image = null, overlay = null;
+	BufferedImage combined;
+	BufferedImage player;
+	File path = new File("Images");
+	static boolean[] b;
+	int i = 0;
+	static int pp = 9; // player position
 
-			try {
-				image = ImageIO.read(new File(path, "full_map.png"));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				overlay = ImageIO.read(new File(path, "room_empty.png"));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				player = ImageIO.read(new File(path, "room_player.png"));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	/**
+	 * The constructor for the map builder.
+	 * @param b : A boolean array used to determine which rooms have been chosen for use
+	 * @param room : An integer correlating the rooms to which room the player is currently in.
+	 */
+	public mapBuilder(boolean[] b, int room) {
 
-			// create the new image, canvas size is the max. of both image sizes
-			int w = Math.max(image.getWidth(), overlay.getWidth());
-			int h = Math.max(image.getHeight(), overlay.getHeight());
-			combined = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-
-			
-			b[9] = true;
-
-			drawImage(-1, -1);
-			if(b[0] == false) drawImage(0,0);	
-			if(b[1] == false) drawImage(90,0);
-			if(b[2] == false) drawImage(180,0);	
-			if(b[3] == false) drawImage(270,0);	
-			if(b[4] == false) drawImage(0,60);	
-			if(b[5] == false) drawImage(90,60);	
-			if(b[6] == false) drawImage(180,60);
-			if(b[7] == false) drawImage(270,60);	
-			if(b[8] == false) drawImage(0,120);
-			if(b[10] == false) drawImage(180,120);
-			if(b[11] == false) drawImage(270,120);
-			if(b[12] == false) drawImage(0,180);
-			if(b[13] == false) drawImage(90,180);
-			if(b[14] == false) drawImage(180,180);
-			if(b[15] == false) drawImage(270,180);
-			
-			if(room == 0) drawPlayer(20, 10);
-			if(room == 1) drawPlayer(110, 10);
-			if(room == 2) drawPlayer(200, 10);
-			if(room == 3) drawPlayer(290, 10);
-			if(room == 4) drawPlayer(20, 70);
-			if(room == 5) drawPlayer(110, 70);
-			if(room == 6) drawPlayer(200, 70);
-			if(room == 7) drawPlayer(290, 70);
-			if(room == 8) drawPlayer(20, 130);
-			if(room == 9) drawPlayer(110,130);
-			if(room == 10) drawPlayer(200, 130);
-			if(room == 11) drawPlayer(290, 130);
-			if(room == 12) drawPlayer(20, 190);
-			if(room == 13) drawPlayer(110, 190);
-			if(room == 14) drawPlayer(200, 190);
-			if(room == 15) drawPlayer(290, 190);
-
+		try {
+			image = ImageIO.read(new File(path, "full_map.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			overlay = ImageIO.read(new File(path, "room_empty.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			player = ImageIO.read(new File(path, "room_player.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
+		// create the new image, canvas size is the max. of both image sizes
+		int w = Math.max(image.getWidth(), overlay.getWidth());
+		int h = Math.max(image.getHeight(), overlay.getHeight());
+		combined = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 
-	public static void main(String[] args){
+		b[9] = true;	//always true, since this is the start room.
+
+		drawImage(-1, -1);	//clear the image if all are true
+		//Draw the rooms that are not in use.
+		if (b[0] == false) drawImage(0, 0);
+		if (b[1] == false) drawImage(90, 0);
+		if (b[2] == false) drawImage(180, 0);
+		if (b[3] == false) drawImage(270, 0);
+		if (b[4] == false) drawImage(0, 60);
+		if (b[5] == false) drawImage(90, 60);
+		if (b[6] == false) drawImage(180, 60);
+		if (b[7] == false) drawImage(270, 60);
+		if (b[8] == false) drawImage(0, 120);
+		if (b[10] == false) drawImage(180, 120);
+		if (b[11] == false) drawImage(270, 120);
+		if (b[12] == false) drawImage(0, 180);
+		if (b[13] == false) drawImage(90, 180);
+		if (b[14] == false) drawImage(180, 180);
+		if (b[15] == false) drawImage(270, 180);
+
+		//Draw the player into hichever room they are in.
+		if (room == 0) drawPlayer(20, 10);
+		if (room == 1) drawPlayer(110, 10);
+		if (room == 2) drawPlayer(200, 10);
+		if (room == 3) drawPlayer(290, 10);
+		if (room == 4) drawPlayer(20, 70);
+		if (room == 5) drawPlayer(110, 70);
+		if (room == 6) drawPlayer(200, 70);
+		if (room == 7) drawPlayer(290, 70);
+		if (room == 8) drawPlayer(20, 130);
+		if (room == 9) drawPlayer(110, 130);
+		if (room == 10) drawPlayer(200, 130);
+		if (room == 11) drawPlayer(290, 130);
+		if (room == 12) drawPlayer(20, 190);
+		if (room == 13) drawPlayer(110, 190);
+		if (room == 14) drawPlayer(200, 190);
+		if (room == 15) drawPlayer(290, 190);
+
+	}
+
+	public static void main(String[] args) {
 		b = new boolean[16];
 		initArray();
 		mapBuilder mb = new mapBuilder(b, pp);
 	}
-	
+
 	private static void initArray() {
 		b[0] = true;
 		b[1] = true;
@@ -120,9 +116,14 @@ public class mapBuilder extends JFrame {
 		b[15] = false;
 	}
 
-
-	public void drawImage(int x, int y){
-		if(x == -1 && y == -1){
+	/**
+	 * This method takes in two coordinates, and draws a brown box that covers up
+	 * the selected room. This will give the total outline of the game.
+	 * @param x : The x coordinate of the room
+	 * @param y : The y coordinate of the room
+	 */
+	public void drawImage(int x, int y) {
+		if (x == -1 && y == -1) {
 			try {
 				ImageIO.write(image, "PNG", new File(path, "combined.png"));
 			} catch (IOException e) {
@@ -130,7 +131,8 @@ public class mapBuilder extends JFrame {
 				e.printStackTrace();
 			}
 		} else {
-			// paint both images, preserving the alpha channels
+			//draw the first image only once, after this,
+			//use the combined image.
 			Graphics g = combined.getGraphics();
 			if (i == 0) {
 				g.drawImage(image, 0, 0, null);
@@ -149,14 +151,21 @@ public class mapBuilder extends JFrame {
 			}
 		}
 	}
-	
-	public void drawPlayer(int x, int y){
 
-		// paint both images, preserving the alpha channels
+	/**
+	 * This method takes in two coordinates, and draws a blue circle into
+	 * the selected room.
+	 * @param x : The x coordinate of the player.
+	 * @param y : The x coordinate of the player.
+	 */
+	public void drawPlayer(int x, int y) {
+
+		//draw the first image only once, after this,
+		//use the combined image.
 		Graphics g = combined.getGraphics();
-		if(i == 0){
+		if (i == 0) {
 			g.drawImage(image, 0, 0, null);
-			i=1;
+			i = 1;
 		} else {
 			g.drawImage(combined, 0, 0, null);
 		}
@@ -164,11 +173,11 @@ public class mapBuilder extends JFrame {
 
 		// Save as new image
 		try {
-			ImageIO.write(combined, "PNG", new File(path, "combined_with_player.png"));
+			ImageIO.write(combined, "PNG", new File(path,
+					"combined_with_player.png"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 }
